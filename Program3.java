bbe76af1ff3c8c1e2c1c872e532d737a8b4367d1
Subhayu Roy <55306904+subhayuroy@users.noreply.github.com>
2020-01-22 22:16:40 +0530

    Update Program3.java
    2nd commit

--- Program3.java
+++ Program3.java
@@ -10,13 +10,13 @@ class Program3 extends Fraction//Use Inheritance
 		System.out.println("Enter numerator and denominator of 2nd fraction:");
 		int n2 = sc.nextInt();//User-Input
 		int d2 = sc.nextInt();//User-Input
-		Fraction ob1= new Fraction(n1,d1);//Object creation
-		Fraction ob2=new Fraction(n2,d2);//Object creation
-		Fraction ob3=new Fraction(0,0);//Object creation
-		Fraction ob4=new Fraction(0,0);//Object creation
-		Fraction ob5=new Fraction(0,0);//Object creation
-		ob4=ob3.addFraction(ob1,ob2);
-		ob5=ob3.reduceFraction(ob4);
+		Fraction ob1 = new Fraction(n1,d1);//Object creation
+		Fraction ob2 = new Fraction(n2,d2);//Object creation
+		Fraction ob3 = new Fraction(0,0);//Object creation
+		Fraction ob4 = new Fraction(0,0);//Object creation
+		Fraction ob5 = new Fraction(0,0);//Object creation
+		ob4 = ob3.addFraction(ob1,ob2);
+		ob5 = ob3.reduceFraction(ob4);
 		System.out.println("Fraction 1: ");//Printing 1st Fraction 
 		ob1.displayFraction();
 		System.out.println("Fraction 2: ");//Printing 2nd Fraction
